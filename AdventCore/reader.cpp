4f529498a1a76a50ba18bd252cb0aa9a68c53c8d
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-01 14:55:42 -0300

    create Reader class

---
+++ AdventCore/reader.cpp
@@ -0,0 +1,52 @@
+﻿#include "reader.h"
+#include <string>
+
+#ifdef  _DEBUG
+#include <direct.h>
+#endif
+
+namespace core
+{
+    std::ifstream _file;
+
+    reader::reader(const char* file_path)
+    {
+        std::ifstream file(file_path);
+        std::string result;
+        std::getline(file, result);
+        std::getline(file, result);
+        file.close();
+        _file.open(file_path);
+
+#ifdef _DEBUG
+        char tuh[_MAX_PATH];
+        char* abc;
+        abc = _getcwd(tuh, sizeof(tuh));
+        std::string str = abc;
+        std::string path = file_path;
+        OutputDebugString(L"Opening File in path: ");
+        OutputDebugString(std::wstring(str.begin(), str.end()).c_str());
+        OutputDebugString(L"\\");
+        OutputDebugString(std::wstring(path.begin(), path.end()).c_str());
+        OutputDebugString(L"\n");
+#endif
+    }
+
+    reader::~reader()
+    {
+        if (_file.is_open())
+        {
+            _file.close();
+        }
+    }
+
+    bool reader::getNextLine(std::string& result)
+    {
+        if (std::getline(_file, result))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ AdventCore/reader.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <iostream>
+#include <fstream>
+#include "config.h"
+
+namespace core
+{
+    class CORE_API reader
+    {
+    public:
+        explicit reader(const char* file_path);
+        ~reader();
+        bool getNextLine(std::string& result);
+    };
+}
