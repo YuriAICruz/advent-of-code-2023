6afc77e8bea5b454a502c4a78147a8f4b284ab82
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-01 16:01:52 -0300

    filter using number spelling

--- AdventCore/calibration.cpp
+++ AdventCore/calibration.cpp
@@ -2,11 +2,104 @@
 
 namespace core
 {
+    const std::string calibration::_numberDictionary[9] = {
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    };
+
     void calibration::addNumber(int* value, int* decimal, int digit)
     {
         *value *= max(1, (*decimal) * 10);
         *value += digit;
         *decimal += 1;
+    }
+
+    bool calibration::isNumber(std::string* data, int& index, int* result)
+    {
+        for (int position = 0, n = sizeof(_numberDictionary)/sizeof(std::string); position < n; ++position)
+        {
+            std::string numberText = _numberDictionary[position];
+            int length = numberText.length();
+            int dataLength = data->length();
+            int count = 0;
+            for (int i = 0; i < length; ++i)
+            {
+                if (numberText[i] != data->at(min(index + i, dataLength-1)))
+                {
+                    break;
+                }
+                count++;
+            }
+            if (count == length)
+            {
+                *result = position+1;
+                return true;
+            }
+        }
+
+        *result = -1;
+        return false;
+    }
+
+    bool calibration::getDigit(std::string* data, int& value, int& decimal, const int& i)
+    {
+        const char e = data->at(i);
+        
+        if (std::isdigit(e))
+        {
+            addNumber(&value, &decimal, e - '0');
+            return true;
+        }
+        return false;
+    }
+
+    bool calibration::getNumber(std::string* data, int& value, int& decimal, int& i)
+    {
+        if (getDigit(data, value, decimal, i))
+        {
+            return true;
+        }
+
+        int digit;
+        if (isNumber(data, i, &digit))
+        {
+            addNumber(&value, &decimal, digit);
+            return true;
+        }
+
+        return false;
+    }
+
+    int calibration::filterDataOnlyDecimals(std::string* data)
+    {
+        int value = 0;
+        int decimal = 0;
+        int length = data->length();
+
+        for (int i = 0; i < length; ++i)
+        {
+            if (getDigit(data, value, decimal, i))
+            {
+                break;
+            }
+        }
+
+        for (int i = length - 1; i >= 0; --i)
+        {
+            if (getDigit(data, value, decimal, i))
+            {
+                break;
+            }
+        }
+
+        return value;
     }
 
     int calibration::filterData(std::string* data)
@@ -18,28 +111,12 @@ namespace core
         char e;
         for (int i = 0; i < length; ++i)
         {
-            e = data->at(i);
-
-            if (!std::isdigit(e))
-            {
-                continue;
-            }
-
-            addNumber(&value, &decimal, e - '0');
-            break;
+            if (getNumber(data, value, decimal, i)) break;
         }
 
         for (int i = length - 1; i >= 0; --i)
         {
-            e = data->at(i);
-
-            if (!std::isdigit(e))
-            {
-                continue;
-            }
-
-            addNumber(&value, &decimal, e - '0');
-            break;
+            if (getNumber(data, value, decimal, i)) break;
         }
 
         return value;
--- AdventCore/calibration.h
+++ AdventCore/calibration.h
@@ -9,9 +9,16 @@ namespace core
     {
     private:
         static void addNumber(int* value, int* decimal, int digit);
+        static bool isNumber(std::string* data, int& i, int* result);
 
     public:
+        static bool getDigit(std::string* data, int& value, int& decimal, const int& i);
+        static bool getNumber(std::string* data, int& value, int& decimal, int& i);
+        static int filterDataOnlyDecimals(std::string* data);
         static int filterData(std::string* data);
         static std::list<int> filterData(std::list<std::string*>* data);
+
+    private:
+        static const std::string _numberDictionary[9];
     };
 }
