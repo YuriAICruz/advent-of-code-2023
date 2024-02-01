5e3c0791b3f6967c6145102a95f43ad71a4b04ff
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-01 14:54:54 -0300

    add to namespace

--- AdventCore/calibration.cpp
+++ AdventCore/calibration.cpp
@@ -1,50 +1,53 @@
 ﻿#include "calibration.h"
 
-void calibration::addNumber(int* value, int* decimal, int digit)
+namespace core
 {
-    *value *= max(1, (*decimal) * 10);
-    *value += digit;
-    *decimal += 1;
-}
-
-int calibration::filterData(std::string* data)
-{
-    int value = 0;
-    int decimal = 0;
-    int length = data->length();
-
-    char e;
-    for (int i = 0; i < length; ++i)
+    void calibration::addNumber(int* value, int* decimal, int digit)
     {
-        e = data->at(i);
-
-        if (!std::isdigit(e))
-        {
-            continue;
-        }
-
-        addNumber(&value, &decimal, e - '0');
-        break;
+        *value *= max(1, (*decimal) * 10);
+        *value += digit;
+        *decimal += 1;
     }
 
-    for (int i = length - 1; i >= 0; --i)
+    int calibration::filterData(std::string* data)
     {
-        e = data->at(i);
+        int value = 0;
+        int decimal = 0;
+        int length = data->length();
 
-        if (!std::isdigit(e))
+        char e;
+        for (int i = 0; i < length; ++i)
         {
-            continue;
+            e = data->at(i);
+
+            if (!std::isdigit(e))
+            {
+                continue;
+            }
+
+            addNumber(&value, &decimal, e - '0');
+            break;
         }
 
-        addNumber(&value, &decimal, e - '0');
-        break;
+        for (int i = length - 1; i >= 0; --i)
+        {
+            e = data->at(i);
+
+            if (!std::isdigit(e))
+            {
+                continue;
+            }
+
+            addNumber(&value, &decimal, e - '0');
+            break;
+        }
+
+        return value;
     }
 
-    return value;
-}
-
-std::list<int> calibration::filterData(std::list<std::string*>* data)
-{
-    auto list = std::list<int>{};
-    return list;
+    std::list<int> calibration::filterData(std::list<std::string*>* data)
+    {
+        auto list = std::list<int>{};
+        return list;
+    }
 }
--- AdventCore/calibration.h
+++ AdventCore/calibration.h
@@ -1,14 +1,17 @@
 ﻿#pragma once
 #include "config.h"
-#include <list> 
+#include <list>
 #include "string"
 
-
-class CORE_API calibration
+namespace core
 {
-private:
-    static void addNumber(int* value, int* decimal, int digit);
-public:
-    static int filterData(std::string* data);
-    static std::list<int> filterData(std::list<std::string*>* data);
-};
+    class CORE_API calibration
+    {
+    private:
+        static void addNumber(int* value, int* decimal, int digit);
+
+    public:
+        static int filterData(std::string* data);
+        static std::list<int> filterData(std::list<std::string*>* data);
+    };
+}
