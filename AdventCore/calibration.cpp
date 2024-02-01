76df5cb79ee65c9e9fbf5f339d9e61c763e24cf7
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-01 11:33:16 -0300

    calibration script created

---
+++ AdventCore/calibration.cpp
@@ -0,0 +1,12 @@
+﻿#include "calibration.h"
+
+int Calibration::filterData(std::string data)
+{
+    return 1122;
+}
+
+std::list<int> Calibration::filterData(std::list<std::string> data)
+{
+    auto list = std::list<int>{};
+    return list;
+}
---
+++ AdventCore/calibration.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <list>
+#include <string>
+
+#pragma once
+#define CORE_EXPORTS
+
+#ifdef CORE_EXPORTS
+#define CORE_API __declspec(dllexport)
+#else
+#define CORE_API __declspec(dllimport)
+#endif
+
+class CORE_API Calibration
+{
+public:
+    static int filterData(std::string data);
+    static std::list<int> filterData(std::list<std::string> data);
+};
