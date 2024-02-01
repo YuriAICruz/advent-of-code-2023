2fd87ad8e7f809ecb09d5bdb355ac8a381e12bb7
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-01 14:56:14 -0300

    update namespaces add Data reading test

--- AdventCoreTests/test.cpp
+++ AdventCoreTests/test.cpp
@@ -1,26 +1,47 @@
 #include "gtest/gtest.h"
 #include "calibration.h"
+#include "reader.h"
 
 TEST(Calibration, FirstEnd)
 {
-  std::string test_value = "1abc2";
-  EXPECT_EQ(12,calibration::filterData(&test_value));
+    std::string test_value = "1abc2";
+    EXPECT_EQ(12, core::calibration::filterData(&test_value));
 }
 
 TEST(Calibration, Middle)
 {
-  std::string test_value = "pqr3stu8vwx";
-  EXPECT_EQ(38,calibration::filterData(&test_value));
+    std::string test_value = "pqr3stu8vwx";
+    EXPECT_EQ(38, core::calibration::filterData(&test_value));
 }
 
 TEST(Calibration, Multiple)
 {
-  std::string test_value = "a1b2c3d4e5f";
-  EXPECT_EQ(15,calibration::filterData(&test_value));
+    std::string test_value = "a1b2c3d4e5f";
+    EXPECT_EQ(15, core::calibration::filterData(&test_value));
 }
 
 TEST(Calibration, Same)
 {
-  std::string test_value = "treb7uchet";
-  EXPECT_EQ(77,calibration::filterData(&test_value));
+    std::string test_value = "treb7uchet";
+    EXPECT_EQ(77, core::calibration::filterData(&test_value));
+}
+
+TEST(Calibration, Spelled)
+{
+    std::string test_value = "two1nine";
+    EXPECT_EQ(29, core::calibration::filterData(&test_value));
+}
+
+TEST(Calibration, FullData)
+{
+    auto read = core::reader{"Data/calibration_input.txt"};
+
+    std::string line;
+    int sum = 0;
+    while (read.getNextLine(line))
+    {
+        sum += core::calibration::filterData(&line);
+    }
+    std::cout << "\n" << sum << "\n";
+    EXPECT_EQ(55123,sum);
 }
