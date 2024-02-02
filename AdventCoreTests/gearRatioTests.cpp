4828d9d12a4606387d857b6031d8811969302ac4
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-02 13:18:34 -0300

    fix tests failing

--- AdventCoreTests/gearRatioTests.cpp
+++ AdventCoreTests/gearRatioTests.cpp
@@ -1,7 +1,24 @@
 ﻿#pragma once
 #include "gtest/gtest.h"
 
-TEST(GearRatio, ab)
+#include "src/reader.h"
+#include "src/GearRatio/gearRatio.h"
+
+TEST(GearRatio, neighboursSum)
 {
-    EXPECT_EQ(1, 0);
+    auto s =
+        "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..";
+    auto ratio = core::gearRatio();
+    ratio.generateGridFromText(s);
+    int sum = ratio.sumRatio();
+    EXPECT_EQ(4361, sum);
+}
+
+TEST(GearRatio, fullData)
+{
+    auto ratio = core::gearRatio();
+    ratio.generateGridFromFile("3_gear_ratio_input.txt");
+    core::gearRatio(s);
+    int sum = ratio.sumRatio();
+    EXPECT_EQ(1, sum);
 }
